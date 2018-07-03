5bc3f582879ec0031b9efc6351b98ec5aeadd2c9
Godsgift Akari <giftakari@yahoo.com>
2018-07-03 19:52:37 +1000

    first Javascript

---
+++ object-101.js
@@ -0,0 +1,8 @@
+let myBook = {title: '1984', author: 'George Orwell',
+pageCount: 326}
+
+console.log('${myBook.title} by ${myBook.author}')
+
+myBook.title = 'Animal farm'
+
+console.log('${myBook.title} by ${myBook.author'})
